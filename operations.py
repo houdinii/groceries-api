f5df76c9c0ee880eb63394fb7e07bddd82ae90a8
Brian Barnes <houdinii1984@gmail.com>
2021-08-05 15:49:26 -0600

    Took methods and functions out of resources and put into operations.

--- operations.py
+++ operations.py
@@ -1,12 +1,12 @@
-from sqlalchemy import asc, desc, func
-from sqlalchemy.sql import label
-from models import Item, Offer
+import json
+from datetime import date, time
+from types import SimpleNamespace
 
 import requests
 import requests_cache
-from requests_cache import CachedSession
-import json
-from types import SimpleNamespace
+from sqlalchemy import exists
+
+from models import Item, Offer
 
 OMIT_LIST = {'images', 'offers', 'user_data'}
 
@@ -73,3 +73,126 @@ def object_or_empty(input_obj, attribute, obj_type):
                 return ""
     else:
         return ""
+
+
+def add_upc_to_db(upc, db):
+    # Create Item object
+    # Create Offer objects
+    # Add or update them
+    new_item = Item(
+        ean=jExtract(upc.items[0], 'ean'),
+        title=jExtract(upc.items[0], 'title'),
+        upc=jExtract(upc.items[0], 'upc'),
+        gtin=jExtract(upc.items[0], 'gtin'),
+        elid=jExtract(upc.items[0], 'elid'),
+        description=jExtract(upc.items[0], 'description'),
+        brand=jExtract(upc.items[0], 'brand'),
+        model=jExtract(upc.items[0], 'model'),
+        color=jExtract(upc.items[0], 'color'),
+        size=jExtract(upc.items[0], 'size'),
+        dimension=jExtract(upc.items[0], 'dimension'),
+        weight=jExtract(upc.items[0], 'weight'),
+        category=jExtract(upc.items[0], 'category'),
+        currency=jExtract(upc.items[0], 'currency'),
+        lowest_recorded_price=jExtract(upc.items[0], 'lowest_recorded_price'),
+        highest_recorded_price=jExtract(upc.items[0], 'highest_recorded_price'),
+    )
+
+    existing_record = db.session.query(Item).filter_by(upc=new_item.upc)
+    exists_bool = db.session.query(exists().where(Item.upc == new_item.upc)).scalar()
+    if exists_bool:
+        db.session.query(Item).filter(Item.upc == new_item.upc).update({
+            "ean": new_item.ean,
+            "title": new_item.title,
+            "upc": new_item.upc,
+            "gtin": new_item.gtin,
+            "elid": new_item.elid,
+            "description": new_item.description,
+            "brand": new_item.brand,
+            "model": new_item.model,
+            "color": new_item.color,
+            "size": new_item.size,
+            "dimension": new_item.dimension,
+            "weight": new_item.weight,
+            "category": new_item.category,
+            "currency": new_item.currency,
+            "lowest_recorded_price": new_item.lowest_recorded_price,
+            "highest_recorded_price": new_item.highest_recorded_price
+        })
+    else:
+        db.session.add(new_item)
+    db.session.commit()
+    new_item_id = db.session.query(Item).filter_by(upc=new_item.upc).first().id
+
+    for each in upc.items[0].offers:
+        new_offer = Offer(
+            item_id=new_item_id,
+            upc=jExtract(each, 'upc'),
+            merchant=jExtract(each, 'merchant'),
+            domain=jExtract(each, 'domain'),
+            title=jExtract(each, 'title'),
+            currency=jExtract(each, 'currency'),
+            list_price=jExtract(each, 'list_price'),
+            price=jExtract(each, 'price'),
+            shipping=jExtract(each, 'shipping'),
+            condition=jExtract(each, 'condition'),
+            availability=jExtract(each, 'availability'),
+            link=jExtract(each, 'link'),
+            updated_t=jExtract(each, 'updated_t'),
+        )
+        existing_record = db.session.query(Offer).filter_by(upc=new_offer.upc).filter(
+            Offer.merchant.contains(new_offer.merchant)).first()
+        if existing_record is None:
+            existing_record_domain = {"domain": "DSFLKJSDFLKDSJFSDLKJFSDLKFJSDFLKJSDLFKJSDLKFJSLDKFJ"}
+        else:
+            existing_record_domain = str(existing_record.domain)
+        if str(existing_record_domain) == str(new_offer.domain):
+            existing_record.item_id = new_offer.item_id
+            existing_record.upc = new_offer.upc
+            existing_record.merchant = new_offer.merchant
+            existing_record.title = new_offer.title
+            existing_record.currency = new_offer.currency
+            existing_record.list_price = new_offer.list_price
+            existing_record.price = new_offer.price
+            existing_record.shipping = new_offer.shipping
+            existing_record.condition = new_offer.condition
+            existing_record.availability = new_offer.availability
+            existing_record.link = new_offer.link
+            existing_record.updated_t = new_offer.updated_t
+        else:
+            db.session.add(new_offer)
+        db.session.commit()
+    return True
+
+
+def serialize(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, date):
+        serial = obj.isoformat()
+        return serial
+
+    if isinstance(obj, time):
+        serial = obj.isoformat()
+        return serial
+
+    return obj.__dict__
+
+
+def jExtract(obj, key):
+    float_list = [
+        "lowest_recorded_price",
+        "highest_recorded_price",
+        "list_price",
+        "price",
+    ]
+    try:
+        attrib = getattr(obj, key)
+        if attrib == '' and key in float_list:
+            return 0
+        return attrib
+    except(KeyError, AttributeError):
+        if key in float_list:
+            return 0
+        else:
+            return ''
